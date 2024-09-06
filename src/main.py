2ca1372a6b4433b6f779103be7e3bd47f221db70
Carfergar97 <cfdezg1997@gmail.com>
2024-09-06 10:31:02 +0200

    Now two equal textnode object are compared

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from textnode import *
 def main():
 
     my_first_text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    my_second_text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
 
+    print(my_first_text_node == my_second_text_node)
     print(my_first_text_node)
+    
 
 if __name__ == "__main__":
     main()
