91597c3079d3da180b545f89cbf3b54d34c9518b
Carfergar97 <cfdezg1997@gmail.com>
2024-09-09 11:16:27 +0200

    added an example code for the parent node

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from textnode import *
+from htmlnode import *
 def main():
 
     my_first_text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -7,6 +8,16 @@ def main():
     print(my_first_text_node == my_second_text_node)
     print(my_first_text_node)
     
+    node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
 
+    print(node.to_html())
 if __name__ == "__main__":
     main()
