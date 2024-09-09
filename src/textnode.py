737eb9c66ec188dd9b4edee23887f0fc0a755d87
Carfergar97 <cfdezg1997@gmail.com>
2024-09-09 17:24:41 +0200

    The text type global variables and the text to html node function have been defined

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from htmlnode import LeafNode
+
+text_type_text  = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode():
     def __init__(self, text, text_type, url = None) -> None:
         self.text = text
@@ -11,3 +20,26 @@ class TextNode():
         
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        
+def text_node_to_html_node(text_node:TextNode) -> LeafNode :
+
+    match text_node.text_type:
+        case text_type_text:
+            return LeafNode(None, text_node.text)
+        
+        case text_type_bold:
+            return LeafNode("b", text_node.text)
+
+        case text_type_italic:
+            return LeafNode("i", text_node.text)
+
+        case text_type_code:
+            return LeafNode("code", text_node.text)
+
+        case text_type_link:
+            return LeafNode("a", text_node.text, {"href":f"{text_node.url}"})
+
+        case text_type_image:
+            return LeafNode("img", props= {"src":f"{self.url}", "alt":f"{text_node.text}"})
+        case _:
+            raise Exception("Text type not known!!!")
