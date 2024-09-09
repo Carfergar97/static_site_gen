ec57aa11c319a3908a8612757543b36b85f23473
Carfergar97 <cfdezg1997@gmail.com>
2024-09-09 10:39:24 +0200

    Change the way to return the props in the HTMLNode parent class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,12 +11,12 @@ class HTMLNode():
         raise NotImplementedError
     
     def props_to_html(self):
-        props_iter = self.props.items()
-        key, value = list(props_iter)[0]
-        attr_str:str = f'{key}="{value}"'
+        if self.props == None:
+            return ""
+        attr_str:str = "" 
 
-        for key, value in list(props_iter)[1:]:
-            attr_str += f' {key}="{value}"'
+        for prop in self.props:
+            attr_str += f' {prop}="{self.props[prop]}"' 
         
         return attr_str
         
@@ -24,26 +24,36 @@ class HTMLNode():
 
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str = None, children: list = None, props: dict = None) -> None:
+        super().__init__(tag, children=children, props=props)
+        
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("Parent nodes musth have a tag!!!")
+        
+        if self.children == None:
+            raise ValueError("Parent nodes must have childrens!!!")
 
-
+        
 class LeafNode(HTMLNode):
     def __init__(self, tag: str = None, value: str = None, props: dict = None) -> None:
         super().__init__(tag, value, props=props)
     
     def to_html(self):
         if self.value == None:
-            raise ValueError
+            raise ValueError("Invalid HTML: no value")
         if self.tag == None:
             return self.value
-        else:
-            if self.tag == "p":
-                return f"<{self.tag}>"+f"{self.value}"+f"</{self.tag}>"
-            elif self.tag == "a":
-                if self.props == None:
-                    raise Exception("There is not hyperlink available.")
-                return f"<{self.tag} " + f"href=" + f'"{self.props["href"]}">' + f"{self.value}" + f"</{self.tag}>"
-            else:
-                raise Exception("This tag is not managed yet...")
+        return  f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+            # if self.tag == "p":
+            #     return f"<{self.tag}>"+f"{self.value}"+f"</{self.tag}>"
+            # elif self.tag == "a":
+            #     if self.props == None:
+            #         raise Exception("There is not hyperlink available.")
+            #     return f"<{self.tag} " + f"href=" + f'"{self.props["href"]}">' + f"{self.value}" + f"</{self.tag}>"
+            # else:
+            #     raise Exception("This tag is not managed yet...")
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
