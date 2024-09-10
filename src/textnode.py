65f00c193eda1bf67bea5ff758361ffa5db2ebff
Carfergar97 <cfdezg1997@gmail.com>
2024-09-10 13:09:57 +0200

    The global variables used to check the text type are deleted

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,5 @@
 from htmlnode import LeafNode
 
-text_type_text  = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
-
 class TextNode():
     def __init__(self, text, text_type, url = None) -> None:
         self.text = text
@@ -24,22 +17,22 @@ class TextNode():
 def text_node_to_html_node(text_node:TextNode) -> LeafNode :
 
     match text_node.text_type:
-        case text_type_text:
+        case "text":
             return LeafNode(None, text_node.text)
         
-        case text_type_bold:
+        case "bold":
             return LeafNode("b", text_node.text)
 
-        case text_type_italic:
+        case "italic":
             return LeafNode("i", text_node.text)
 
-        case text_type_code:
+        case "code":
             return LeafNode("code", text_node.text)
 
-        case text_type_link:
+        case "link":
             return LeafNode("a", text_node.text, {"href":f"{text_node.url}"})
 
-        case text_type_image:
-            return LeafNode("img", props= {"src":f"{self.url}", "alt":f"{text_node.text}"})
+        case "image":
+            return LeafNode("img", props= {"src":f"{text_node.url}", "alt":f"{text_node.text}"})
         case _:
             raise Exception("Text type not known!!!")
