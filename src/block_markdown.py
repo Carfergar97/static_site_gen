43a6cb1c07ed6640d5f00d3864f987c1160c0b0b
Carfergar97 <cfdezg1997@gmail.com>
2024-09-13 18:28:45 +0200

    Added the empty block cleaning to the markdown to blocks function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,21 +1,20 @@
 
-def markdownt_to_blocks(markdown:str) -> list:
+def markdown_to_blocks(markdown:str) -> list:
     if markdown == "":
         return [""]
     
     blocks_lst = markdown.split("\n")
-
-    for line in blocks_lst:
-        line.strip()
-        if line == "":
-            blocks_lst.remove(line)
-
+    for i in range(blocks_lst.count("")):
+        blocks_lst.remove("")
+    for line in blocks_lst: 
+        blocks_lst[blocks_lst.index(line)] = line.strip()
+        
     return blocks_lst
 
 
 if __name__ == "__main__":
 
-    print(markdownt_to_blocks("# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\
+    print(markdown_to_blocks("# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\
     * This is the first list item in a list block\n\
     * This is a list item\n\
-    * This is another list item"))
+    * This is another list item            "))
