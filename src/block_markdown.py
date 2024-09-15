74839edb80552a1104d4efa5bce3faba485f9334
Carfergar97 <cfdezg1997@gmail.com>
2024-09-15 12:46:20 +0200

    Added the block_to_block_type function to the block_markdown file

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -11,6 +11,19 @@ def markdown_to_blocks(markdown:str) -> list:
         
     return blocks_lst
 
+def block_to_block_type(markdown:str) -> str:
+    if(markdown.split("#").count("") != 0 and markdown[markdown.split("#").count("")]==" "):
+        return "heading"
+    elif(markdown[:3] == "```" and markdown[-3:] == "```"):
+        return "code"
+    elif(markdown[0] == ">"):
+        return "quote"
+    elif(markdown[:2] == "- " or markdown[:2] == "* "):
+        return "unordered_list"
+    elif(markdown[0].isdecimal and markdown[1:3] == ". "): 
+        return "ordered_list"
+    else:
+        return "paragraph"
 
 if __name__ == "__main__":
 
