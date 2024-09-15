101e43f2f03a9c198e84e3e1175a465bfc7de109
Carfergar97 <cfdezg1997@gmail.com>
2024-09-15 19:43:15 +0200

    deleted the space between the list items

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
 
     print(markdown_to_blocks("# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n\
     * This is the first list item in a list block\n\
-    * This is a list item\n\
-    * This is another list item            "))
+* This is a list item\n\
+* This is another list item            "))
